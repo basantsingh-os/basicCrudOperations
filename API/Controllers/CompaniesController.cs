e3ac06e1eae2994bd72aa48f034bb77c92cb13e3
Basant Singh <you@example.com>
2020-05-28 23:49:56 +0530

    basic controller

---
+++ API/Controllers/CompaniesController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CompaniesController : ControllerBase
+    {
+        [HttpGet]
+        public string GetCompanies()
+        {
+            return "list of all companies";
+        }
+
+        [HttpGet("{id}")]
+        public string GetCompany(int id)
+        {
+            return "single company";
+        }
+
+        
+        
+    }
+}
