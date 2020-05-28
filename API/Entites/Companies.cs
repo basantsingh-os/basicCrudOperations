18f5804478dde58d5ac5ad56a096a4f4ffc34f79
Basant Singh <you@example.com>
2020-05-28 23:54:12 +0530

    entity definition

---
+++ API/Entites/Companies.cs
@@ -0,0 +1,10 @@
+namespace API
+{
+    public class Companies
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public bool PaymentMade { get; set; }
+    }
+}
