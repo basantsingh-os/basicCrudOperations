40407647a56aba7783304cfe1a688c5ca740495d
Basant Singh <you@example.com>
2020-05-30 17:17:30 +0530

    3 projects interdependant on each other

--- API/Controllers/CompanysController.cs
+++ API/Controllers/CompanysController.cs
@@ -1,7 +1,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using API.Data;
+using Core;
+using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
